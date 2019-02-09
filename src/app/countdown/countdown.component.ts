981e7c784ab553b451884f92bc176c5bad753c5a
mitchelawesome <mitchel@austinmedia.nl>
2019-02-09 14:56:45 +0100

    added onchages

--- src/app/countdown/countdown.component.ts
+++ src/app/countdown/countdown.component.ts
@@ -1,25 +1,35 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnDestroy, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-countdown',
   templateUrl: './countdown.component.html',
   styleUrls: ['./countdown.component.scss']
 })
-export class CountdownComponent implements OnInit {
+export class CountdownComponent implements OnInit, OnDestroy, OnChanges {
 
   @Output() onDecrease = new EventEmitter<number>();
   @Output() onComplete = new EventEmitter<void>();
-  @Input() init:number = null;
-  counter:number = 0;
-  private countdownTimerRef:any = null;
+
+  @Input() init: number = null;
+  counter = 0;
+  private countdownTimerRef: any = null;
   constructor() { }
 
   ngOnInit() {
     this.startCountdown();
   }
 
+  ngOnDestroy(): void {
+    this.clearTimeout();
+  }
+
+  ngOnChanges(changes): void {
+    console.log(`-- init value changed to ${changes.init.currentValue}`);
+    this.startCountdown();
+  }
+
   startCountdown() {
-    if(this.init && this.init > 0) {
+    if (this.init && this.init > 0) {
       this.clearTimeout();
       this.counter = this.init;
       this.doCountdown();
@@ -27,15 +37,15 @@ export class CountdownComponent implements OnInit {
   }
 
   doCountdown() {
-    this.countdownTimerRef = setTimeout(()=>{
+    this.countdownTimerRef = setTimeout(() => {
       this.counter = this.counter - 1;
-      this.processCountdown()
-    }, 1000)
+      this.processCountdown();
+    }, 1000);
   }
 
   private clearTimeout() {
     console.log('*** Clear timeout INIT ***');
-    if(this.countdownTimerRef) {
+    if (this.countdownTimerRef) {
       clearTimeout(this.countdownTimerRef);
       this.countdownTimerRef = null;
       console.log('*** Clear timeout called ***');
@@ -46,9 +56,9 @@ export class CountdownComponent implements OnInit {
     this.onDecrease.emit(this.counter);
     console.log('the current count is ', this.counter);
 
-    if(this.counter == 0) {
+    if (this.counter === 0) {
       this.onComplete.emit();
-      console.log("-- counter end --");
+      console.log('-- counter end --');
     } else {
       this.doCountdown();
     }
